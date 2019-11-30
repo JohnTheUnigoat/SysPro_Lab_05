59fe55cfea0c3a21748e74c18b923a403eea6a76
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-11-30 22:42:30 +0200

    Created Department class
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

---
+++ SysPro_Lab_05/Classes/Department.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysPro_Lab_05.Classes
+{
+    public class Department
+    {
+        public string Name { get; set; }
+
+        private string phoneNumber;
+        public string PhoneNumber {
+            get
+            {
+                return phoneNumber;
+            }
+            set
+            {
+                if (value.Length != 10)
+                    throw new ArgumentException("A phone number should be exactly 10 digits long!");
+
+                if (!value.All(symbol => char.IsDigit(symbol)))
+                    throw new ArgumentException("A phone number can only contain digits!");
+
+                phoneNumber = value;
+            }
+        }
+
+        public string Address { get; set; }
+
+        public Department(string name, string phoneNumber, string address)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Address = address;
+        }
+    }
+}
