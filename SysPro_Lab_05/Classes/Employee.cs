cb911e9a215df99e8d6eb5bf3cd59098bf18a5a0
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-11-30 23:34:42 +0200

    Created Employee class
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

---
+++ SysPro_Lab_05/Classes/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysPro_Lab_05.Classes
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public decimal Salary { get; set; }
+
+        private Department department;
+
+        public string DepartmentName
+        {
+            get
+            {
+                return department.Name;
+            }
+        }
+
+        public string DepartmentPhone
+        {
+            get
+            {
+                return department.PhoneNumber;
+            }
+        }
+
+        public string DepartmentAdress
+        {
+            get
+            {
+                return department.Address;
+            }
+        }
+
+        public Employee(string name, int age, decimal salary, Department department)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+            this.department = department;
+        }
+    }
+}
