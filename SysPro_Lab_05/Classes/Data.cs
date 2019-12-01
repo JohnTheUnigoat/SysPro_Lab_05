df87e91500239241df97ff6668a28b85854928a8
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-12-01 02:15:50 +0200

    Created Data class
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

---
+++ SysPro_Lab_05/Classes/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysPro_Lab_05
+{
+    public class Data
+    {
+        public List<Department> Departments { get; set; }
+
+        public List<Employee> Employees { get; set; }
+
+        public Data()
+        {
+            Departments = new List<Department>();
+            Employees = new List<Employee>();
+        }
+    }
+}
